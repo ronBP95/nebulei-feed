d393aa6e71cfe0a99ceec1255ec2b073a1ea111a
Adrian Blanc <lalo@physicslab.space>
2022-10-13 16:15:38 -0500

    added profile container 👥

---
+++ src/container/Profile/Profile.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Profile() {
+
+  return (
+    <>
+    <section className="">
+        Profile
+    </section>
+    </>
+  );
+};
+
+
+export default Profile;
