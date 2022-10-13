769f95c9b3e59f8445f94d4d801b1f1da36826ab
Adrian Blanc <lalo@physicslab.space>
2022-10-13 16:15:10 -0500

    boilerplate of feed 🗃️

--- src/container/Feed/Feed.tsx
+++ src/container/Feed/Feed.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 
 
 function Feed() {
 
   return (
     <>
+    <Helmet>
+        <title>Feed | Nebulei</title>
+        <meta name="description" content=""/>
+        <meta name="keywords" content=""/>
+    </Helmet>
+
     <section className="">
-        Feed
+        Feed Page!
     </section>
     </>
   );
 };
+
 
 export default Feed;
